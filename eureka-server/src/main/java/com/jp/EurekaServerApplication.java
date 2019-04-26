89f7e31133a7c85b18efbf105cb287e65e84012d
jipeng <jipeng@beeplay123.com>
2019-04-26 08:24:49 +0800

    # 1

---
+++ eureka-server/src/main/java/com/jp/EurekaServerApplication.java
@@ -0,0 +1,10 @@
+package com.jp;
+
+/**
+ * @Author: jipeng
+ * @Description:
+ * @Date: Created in 2019/4/26 7:48
+ */
+@SpringBootApplication
+public class EurekaServerApplication {
+}
---
+++ spring-boot-demo/src/main/java/com/jp/springbootdemo/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.jp.springbootdemo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: jipeng
+ * @Description:
+ * @Date: Created in 2019/4/24 8:04
+ */
+@RestController
+public class HelloController {
+
+    @Value("${book.name}")
+    private String name;
+
+    @Value("${book.author}")
+    private String author;
+
+    @Value("${book.desc}")
+    private String desc;
+
+    @RequestMapping("/hello")
+    public Object hello(){
+        return "Hello World ; desc: " + this.desc ;
+    }
+
+}
