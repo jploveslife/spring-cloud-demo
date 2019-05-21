f6d43048ee0f74e0355cd7015cdd93b168e94a26
jipeng <jipeng@beeplay123.com>
2019-05-22 07:20:21 +0800

    # 1

--- api-gateway/src/main/java/com/jp/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/jp/ApiGatewayApplication.java
@@ -2,10 +2,7 @@ package com.jp;
 
 import com.jp.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
